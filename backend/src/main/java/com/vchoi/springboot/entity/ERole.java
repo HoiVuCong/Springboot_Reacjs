39c4f14ae1edb54ae8d8682bb98a9e1462ccf365
vchoi <vchoi@cmc.com.vn>
2021-05-28 16:34:19 +0700

    role e num

---
+++ backend/src/main/java/com/vchoi/springboot/entity/ERole.java
@@ -0,0 +1,7 @@
+package com.vchoi.springboot.entity;
+
+public enum ERole {
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_MODERATOR
+}
--- backend/src/main/java/com/vchoi/springboot/entity/Role.java
+++ backend/src/main/java/com/vchoi/springboot/entity/Role.java
@@ -1,8 +1,16 @@
 package com.vchoi.springboot.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String role;
+
+	@Enumerated(EnumType.STRING)
+	private ERole role;
 	
 }
